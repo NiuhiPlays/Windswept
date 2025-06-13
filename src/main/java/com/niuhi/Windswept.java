c4ffb0166188f3ebd24c8248627687f4879313f3
NiuhiPlays <nielsiansmeets@gmail.com>
2025-06-13 22:26:43 +0200

    Combined the Initializers into 1

--- src/main/java/com/niuhi/Windswept.java
+++ src/main/java/com/niuhi/Windswept.java
@@ -1,24 +1,28 @@
 package com.niuhi;
 
+import com.niuhi.debug.DebugCommands;
 import com.niuhi.particle.player.*;
-import com.niuhi.particle.player.PlayerParticleTypes;
 import com.niuhi.particle.water.*;
-import com.niuhi.particle.water.WaterParticleTypes;
 import com.niuhi.particle.weather.wind.WindParticle;
 import com.niuhi.particle.weather.wind.WindParticleTypes;
+import com.niuhi.player.FootprintSystem;
+import com.niuhi.player.RunningCloudsSystem;
+import com.niuhi.sounds.AmbientSoundSystem;
+import com.niuhi.sounds.SoundEvents;
+import com.niuhi.water.*;
 import com.niuhi.weather.wind.WindSystem;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Windswept implements ModInitializer {
+public class Windswept implements ClientModInitializer {
 	public static final String MOD_ID = "windswept";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static WindSystem WIND_SYSTEM = null; // Initialize to null, set in WindsweptClient
+	public static WindSystem WIND_SYSTEM = null; // Initialize to null, set during registration
 
-    @Override
-	public void onInitialize() {
+	@Override
+	public void onInitializeClient() {
 		LOGGER.info("Blowing in the Clouds and stuff!");
 
 		// Wind Particles
@@ -34,7 +38,7 @@ public class Windswept implements ModInitializer {
 		ParticleFactoryRegistry.getInstance().register(WaterParticleTypes.WAVE, WaveParticle.Factory::new);
 		ParticleFactoryRegistry.getInstance().register(WaterParticleTypes.FOAM, FoamParticle.Factory::new);
 
-		//Player Particles
+		// Player Particles
 		PlayerParticleTypes.registerParticles();
 		ParticleFactoryRegistry.getInstance().register(PlayerParticleTypes.FOOTPRINT, FootprintParticle.Factory::new);
 		ParticleFactoryRegistry.getInstance().register(PlayerParticleTypes.FOOTPRINT_WET, FootprintParticle.Factory::new);
@@ -43,5 +47,25 @@ public class Windswept implements ModInitializer {
 		ParticleFactoryRegistry.getInstance().register(PlayerParticleTypes.FOOTPRINT_SAND, FootprintParticle.Factory::new);
 		ParticleFactoryRegistry.getInstance().register(PlayerParticleTypes.FOOTPRINT_REDSAND, FootprintParticle.Factory::new);
 		ParticleFactoryRegistry.getInstance().register(PlayerParticleTypes.DUST_CLOUD, DustCloudParticle.Factory::new);
+
+		// Water Systems
+		CascadeSystem.register();
+		RippleSystem.register();
+		WaveSystem.register();
+		SplashSystem.register();
+
+		// Player Systems
+		FootprintSystem.register();
+		RunningCloudsSystem.register();
+
+		// Weather Systems
+		WindSystem.register(); // Initialize WIND_SYSTEM here if needed
+
+		// Sound Events
+		SoundEvents.initialize();
+		AmbientSoundSystem.register();
+
+		// Debug
+		DebugCommands.register();
 	}
 }
